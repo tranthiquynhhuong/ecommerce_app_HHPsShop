a42d82be5f35b1f44381bab3b5923aaf21bcea48
= <=>
2019-12-21 15:32:43 +0700

    Err Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_shop_flutter/views/SplashScreen.dart';
 import 'bloc/ThemeBloc.dart';
+import 'bloc/UserBloc.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with AfterLayoutMixin {
+  final _themeBloc = ThemeBloc();
+  final _userBloc = UserBloc();
+
+  @override
+  void afterFirstLayout(BuildContext context) async {
+    await _userBloc.getAutoLoginValuesSF();
+    await _themeBloc.getThemeValuesSF();
+    // TODO: implement afterFirstLayout
+  }
+
   @override
   Widget build(BuildContext context) {
-    final themeBloc = ThemeBloc();
-
     return StreamBuilder(
-      stream: themeBloc.observableTheme,
+      stream: _themeBloc.observableTheme,
       initialData: false,
       builder: (context, snapshot) {
         if(!snapshot.hasData|| snapshot.data == null) return Container(color: Colors.red,);
@@ -26,4 +41,6 @@ class MyApp extends StatelessWidget {
       }
     );
   }
+
+
 }
